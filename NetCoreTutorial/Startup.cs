6ebb5d68a7dc69644cb2e210def9bfba23ccd908
Vlad <Birziloiu>
2020-11-13 15:37:24 +0200

    Up until HTML FORMS

--- NetCoreTutorial/Startup.cs
+++ NetCoreTutorial/Startup.cs
@@ -21,20 +21,23 @@ namespace NetCoreTutorial
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
+            //comment pt ca e cretin tutorialul
+            //if (env.IsDevelopment())
+            //{
+            //    app.UseDeveloperExceptionPage();
+            //}
 
-            app.UseRouting();
+            //app.UseRouting();
 
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
-            });
+            //app.UseEndpoints(endpoints =>
+            //{
+            //    endpoints.MapGet("/", async context =>
+            //    {
+            //        await context.Response.WriteAsync("Hello World!");
+            //    });
+            //});
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
         }
     }
 }
